63eb0222b64e05f0d1d4a1c5e1eb6eb1d4450160
jffry93 <jay.zalischi@gmail.com>
2021-03-03 19:48:44 -0500

    updated homepage
     changed navbar. adjusted reel and resume containers

--- dist/bootstrap.min.fdb78abe.js
+++ dist/bootstrap.min.fdb78abe.js
@@ -15678,7 +15678,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54196" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53382" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
