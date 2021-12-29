d52fac559d5050282d6d1d5e59804c0ab9c1cb3d
Jeffrey Zalischi <jay.zalischi@gmail.com>
2021-12-28 19:42:27 -0500

    set up about page to use gsap scroll trigger

---
+++ src/js/gsapScrollTrigger.js
@@ -0,0 +1,13 @@
+//Welcome Heading
+gsap.to('.intro-welcome', {
+  scrollTrigger: '.intro-welcome',
+  x: 0,
+  duration: 0.7,
+});
+
+//billingual
+// gsap.to('.second-container', {
+//   scrollTrigger: '.second-container',
+//   x: 500,
+//   duration: 2,
+// });
